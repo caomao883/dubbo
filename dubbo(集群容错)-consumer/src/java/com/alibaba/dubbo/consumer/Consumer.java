12cc1731f84428faa90fe11112ea946a4701d8c5
ouhuihao <862162990@qq.com>
2018-02-24 00:22:16 +0800

    集群容错
    Signed-off-by: ouhuihao <862162990@qq.com>

---
+++ dubbo(集群容错)-consumer/src/java/com/alibaba/dubbo/consumer/Consumer.java
@@ -0,0 +1,21 @@
+package com.alibaba.dubbo.consumer;
+
+import com.alibaba.dubbo.demo.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Consumer {
+    public static void main(String[] args) {
+        //测试常规服务
+        //加载spring配置
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("consumer.xml");
+        context.start();
+        System.out.println("consumer start");
+        DemoService demoService = context.getBean(DemoService.class);
+        System.out.println("consumer");
+        //打印代理类型，底层dubbo使用NIO的RCP做远程调用
+        System.out.println("打印引用类型:"+demoService.getClass());
+        System.out.println(demoService.mysum(3,4));
+
+    }
+}
---
+++ dubbo(集群容错1)-provider/src/java/com/alibaba/dubbo/demo/impl/DemoServiceImpl.java
@@ -0,0 +1,15 @@
+package com.alibaba.dubbo.demo.impl;
+
+import com.alibaba.dubbo.demo.DemoService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoServiceImpl implements DemoService {
+
+    @Override
+    public int mysum(int a, int b) {
+        return Integer.parseInt(Integer.toString(a)+Integer.toString(b)+100);
+    }
+
+}
